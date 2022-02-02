de95e7a6fa55905759095af0b56bb0ecd24db5fe
Your Name <kasifatmolla0@gmail.com>
2022-02-02 09:13:03 +0600

    First commit

---
+++ Task1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<body>
+ <h1> Registration Form </h1>
+ <form action="action_page.php">
+     <table>
+         <tr>
+             <td> First Name: </td>
+             <td> <input type="text"></td> </tr>
+             <tr>
+             <td> Last Name: </td>
+             <td> <input type="text"></td> </tr>
+             <tr>
+             <td> Age: </td>
+             <td> <input type="number"></td> </tr> 
+             <tr>
+             <td> Designation: </td>
+             <td> 
+                 <input type="radio" name:"designation">Junior Programmer
+                 <input type="radio" name:"designation">Senior Programmer
+                 <input type="radio" name:"designation">Project Lead
+            </td> </tr>
+            <tr>
+            <td> Preferred language: </td>
+             <td> 
+                 <input type="checkbox" name:"language">JAVA
+                 <input type="checkbox" name:"language">PHP
+                 <input type="checkbox" name:"language">C++
+            </td> </tr>
+            <tr>
+             <td> E-mail: </td>
+             <td> <input type="email"></td> </tr>
+             <tr>
+             <td> Password: </td>
+             <td> <input type="password"></td> </tr>
+             <tr>
+             <td> Please choose a file </td>
+             <td> 
+             <input type="file">
+            </td> </tr>
+</table>
+<input type="Submit" value="Submit">
+<input type="Reset" value="Reset">
+ </body>
+</html>
